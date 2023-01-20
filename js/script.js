8b4d5a6ded4c41178881deb60562d460a4ffec33
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-01-20 20:27:42 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -9,22 +9,48 @@
             done: true,
         },
     ];
-    
+
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
-            <li>
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
-            </li>`;
+            </li>
+            `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+
+    const addNewTask = () => {
+        tasks.push({
+            content: newTaskContent,
+         });
+
+         render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+     if(newTaskContent === "") {
+        return;
+     }
+
+     addNewTask(newTaskContent);
+    };
+
     const init = () => {
-render();
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
